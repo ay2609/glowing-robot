4cf350320eeaf2bc91700619ef2e58325719c403
ay2609 <ayadava@ideaventionsacademy.org>
2024-02-12 16:28:41 -0500

    fix identify face again

--- glowing_robot/identify.py
+++ glowing_robot/identify.py
@@ -1,13 +1,21 @@
 import numpy as np
 
 from .face import Face
-from .database import get_db
+from .database import get_db, get_profile, Profile
 from .cosine_similarity import determine_similarity
 
 
-def identify_face(face: Face):
+def identify_face(face: Face) -> str:
     name_to_descriptor = np.array([(profile.mean, name) for name, profile in get_db().items])
     bool_to_similarity = determine_similarity(face.descriptor, name_to_descriptor[:, 0])
 
     best_descriptors = zip(name_to_descriptor[bool_to_similarity[:, 1]], bool_to_similarity[bool_to_similarity[:, 0]][:, 1])
-    return best_descriptors
+
+    if not best_descriptors:
+        best_person = max(best_descriptors, key=lambda x: x[1])
+        best_profile = get_profile(best_person[0])
+        face.profile = best_profile
+        best_profile.add_descriptors(best_person[1])
+        return best_person[0]
+    else:
+        return "unknown"
