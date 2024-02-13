528e0466e640362d23c8ddb6fb17f658ad90156c
ay2609 <ayadava@ideaventionsacademy.org>
2024-02-12 22:53:37 -0500

    fix avi stupidity

--- glowing_robot/identify.py
+++ glowing_robot/identify.py
@@ -6,7 +6,7 @@ from .cosine_similarity import determine_similarity
 
 
 def identify_face(face: Face) -> None:
-    name_to_descriptor = np.array([(profile.mean, name) for name, profile in get_db().items])
+    name_to_descriptor = np.array([(profile.mean, name) for name, profile in get_db().items()])
     bool_to_similarity = determine_similarity(face.descriptor, name_to_descriptor[:, 0])
 
     best_descriptors = zip(name_to_descriptor[bool_to_similarity[:, 1]], bool_to_similarity[bool_to_similarity[:, 0]][:, 1])
